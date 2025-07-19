71bdeb58e7bd17106c5410ebd12fac427f87fcb2
Kushal J <jkushal208@gmail.com>
2025-07-19 21:00:27 +0530

    Site Upadate withe Gsap

---
+++ src/script.js
@@ -0,0 +1,73 @@
+var tl= gsap.timeline({scrollTrigger:{
+  trigger:"#two",
+  start:"5% 95%",
+  end:"50% 50%",
+  scrub:true,
+  markers:false,
+}})
+
+tl.to("#fanta",{
+  top:"125%",
+  left:"14%",
+  width:"20%"
+},orange)
+tl.to("#orange",{
+  top:"160%",
+  left:"25%"
+},orange)
+
+tl.to("#leaf",{
+  top:"125%",
+  left:"73%",
+  rotate:"330"
+},orange)
+
+tl.to("#orange2",{
+  top:"171%",
+  right:"3%",
+  width:"15%"
+},orange)
+
+
+var tl2=gsap.timeline({scrollTrigger:{
+  trigger:"#three",
+  start:"0% 80%",
+  end:"50% 50%",
+  markers:true,
+  scrub:true
+}})
+
+
+tl2.from("#lemonslice",{
+  rotate:"240",
+  top:"0%"
+},"<")
+tl2.from("#pepsilogo",{
+  rotate:"360",
+  top:"0%"
+},"<  ")
+
+tl2.from("#spritecan", {
+  rotate: -15,
+  opacity:0
+},"<");
+
+tl2.from("#pepsican", {
+  rotate: 15,
+  opacity:0
+},"<");
+
+
+tl2.to("#fanta",{
+  top:"221%",
+  left:"40.5%",
+  width:"18%"
+},"<")
+
+tl2.to("#orange",{
+  top:"202%",
+  left:"40%",
+  rotate:360
+
+},"<")
+
